30e26b29f756047f80f348e2377e534b3605e9ce
lossyrob <rdemanuele@gmail.com>
2016-07-05 14:11:16 -0400

    Download from landsat-pds

--- data/landsat/download-data.sh
+++ data/landsat/download-data.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
-wget https://s3.amazonaws.com/geotrellis-sample-datasets/landsat/LC80140322014139LGN00.tar.bz
-tar xvfj LC80140322014139LGN00.tar.bz
+wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_B4.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_B5.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_BQA.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_MTL.txt
+
+# https://s3.amazonaws.com/geotrellis-sample-datasets/landsat/LC80140322014139LGN00.tar.bz
+# tar xvfj LC80140322014139LGN00.tar.bz
