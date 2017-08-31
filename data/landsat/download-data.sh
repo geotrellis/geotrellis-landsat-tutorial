043bd3125817f35f3b945285389849356e898d4f
Eugene Cheipesh <echeipesh@gmail.com>
2017-08-30 21:07:25 -0400

    Prepare two scenes

--- data/landsat/download-data.sh
+++ data/landsat/download-data.sh
@@ -6,5 +6,12 @@ wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC8107
 wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_BQA.TIF
 wget http://landsat-pds.s3.amazonaws.com/L8/107/035/LC81070352015218LGN00/LC81070352015218LGN00_MTL.txt
 
+
+wget http://landsat-pds.s3.amazonaws.com/L8/107/036/LC81070362015218LGN00/LC81070362015218LGN00_B3.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/036/LC81070362015218LGN00/LC81070362015218LGN00_B4.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/036/LC81070362015218LGN00/LC81070362015218LGN00_B5.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/036/LC81070362015218LGN00/LC81070362015218LGN00_BQA.TIF
+wget http://landsat-pds.s3.amazonaws.com/L8/107/036/LC81070362015218LGN00/LC81070362015218LGN00_MTL.txt
+
 # https://s3.amazonaws.com/geotrellis-sample-datasets/landsat/LC80140322014139LGN00.tar.bz
 # tar xvfj LC80140322014139LGN00.tar.bz
--- src/main/scala/tutorial/MaskBandsRandGandNIR.scala
+++ src/main/scala/tutorial/MaskBandsRandGandNIR.scala
@@ -6,30 +6,33 @@ import geotrellis.raster.render._
 import com.typesafe.config.ConfigFactory
 
 object MaskBandsRandGandNIR {
-  val maskedPath = "data/r-g-nir.tif"
+
   //constants to differentiate which bands to use
   val R_BAND = 0
   val G_BAND = 1
   val NIR_BAND = 2
   // Path to our landsat band geotiffs.
-  def bandPath(b: String) = s"data/landsat/LC81070352015218LGN00_${b}.TIF"
+  def bandPath(b: String, path: String, row: String) = s"data/landsat/LC8${path}${row}2015218LGN00_${b}.TIF"
 
-  def main(args: Array[String]): Unit = {
+  def combineBands(path: String, row: String): Unit = {
+    val maskedPath = s"data/r-g-nir-$path-$row.tif"
+    println(s"Generating $maskedPath")
+
     // Read in the red band
     println("Reading in the red band...")
-    val rGeoTiff = SinglebandGeoTiff(bandPath("B4"))
+    val rGeoTiff = SinglebandGeoTiff(bandPath("B4", path, row))
 
     // Read in the green band
     println("Reading in green band...")
-    val gGeoTiff = SinglebandGeoTiff(bandPath("B3"))
+    val gGeoTiff = SinglebandGeoTiff(bandPath("B3", path, row))
 
     // Read in the near infrared band
     println("Reading in the NIR band...")
-    val nirGeoTiff = SinglebandGeoTiff(bandPath("B5"))
+    val nirGeoTiff = SinglebandGeoTiff(bandPath("B5", path, row))
 
     // Read in the QA band
     println("Reading in the QA band...")
-    val qaGeoTiff = SinglebandGeoTiff(bandPath("BQA"))
+    val qaGeoTiff = SinglebandGeoTiff(bandPath("BQA", path, row))
 
     // GeoTiffs have more information we need; just grab the Tile out of them.
     val (rTile, gTile, nirTile, qaTile) = (rGeoTiff.tile, gGeoTiff.tile, nirGeoTiff.tile, qaGeoTiff.tile)
@@ -57,5 +60,10 @@ object MaskBandsRandGandNIR {
     // Create a multiband geotiff from our tile, using the same extent and CRS as the original geotiffs.
     println("Writing out the multiband R + G + NIR tile...")
     MultibandGeoTiff(mb, rGeoTiff.extent, rGeoTiff.crs).write(maskedPath)
+  }
+
+  def main(args: Array[String]): Unit = {
+    combineBands(path = "107",  row = "035")
+    combineBands(path = "107",  row = "036")
   }
 }
